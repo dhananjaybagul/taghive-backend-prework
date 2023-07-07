fbad8e35b0760e7952e9cea7877cd0a0d165ce3e
dhananjaybagul <dhananjaybagul07@gmail.com>
2023-07-07 12:31:32 +0530

    Initialised node project and installed required packages

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { banner } from "./src/logger/banner.js";
+import logger from "./src/logger/logger.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from './src/api/Auth/AuthRoutes.js';
+
+import("./src/database/dbconfig.js");
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+
+
+app.use("*", function (req, res) {
+  res.status(404).send("404");
+});
+
+let port = process.env.port || 3000;
+
+app.listen(port, () => {
+  banner(logger);
+});
